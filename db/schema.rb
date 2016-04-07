2171c11fd62352b9f07495af6d23f8edccaedd7c
Urabe, Shyouhei <shyouhei@ruby-lang.org>
2016-04-07 18:02:32 +0900

    bin/rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160404105755) do
+ActiveRecord::Schema.define(version: 20160407054125) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "namespace"
+    t.integer  "number"
+    t.integer  "post_id"
+    t.text     "url"
+    t.integer  "revision_number",  default: 1
+    t.integer  "stargazers_count", default: 0
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  add_index "comments", ["namespace", "number"], name: "index_comments_on_namespace_and_number", unique: true
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "namespace"
